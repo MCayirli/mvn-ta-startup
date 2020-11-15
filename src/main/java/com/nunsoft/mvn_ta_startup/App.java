4019eb58a768e583b0e711920dfa568534f16ff6
MCayirli <cayirli@gmail.com>
2020-11-15 10:50:39 +0300

    A sample test automation java code has been added.

--- src/main/java/com/nunsoft/mvn_ta_startup/App.java
+++ src/main/java/com/nunsoft/mvn_ta_startup/App.java
@@ -1,13 +1,21 @@
 package com.nunsoft.mvn_ta_startup;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+	public static void main(String[] args) {
+		//System.out.println("Hello World!");
+		System.setProperty("webdriver.chrome.driver", "webdriver\\chromedriver.exe");
+
+		// Navigate to http://www.google.com
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.google.com");
+
+		// Enter the key "Hello World" to search field
+		WebElement q = driver.findElement(By.xpath("//input[@name = 'q']"));
+		q.sendKeys("Hello World");
+	}
 }
